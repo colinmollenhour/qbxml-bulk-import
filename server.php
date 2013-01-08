c2658aa0caeff9dfe7e8b91db5aa7167865d2e43
colinmollenhour <colin@mollenhour.com>
2013-01-08 00:36:14 -0500

    Tested and working.

--- server.php
+++ server.php
@@ -1,138 +1,203 @@
 <?php
+if (isset($_GET['pi'])) { echo phpinfo(); exit; }
+define('BP', __DIR__);
+define('VAR_DIR', __DIR__.'/var');
 
-$configPath = __DIR__.'/config.json';
+$proto = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" ? 'https' : 'http');
+$host = $_SERVER['HTTP_HOST'];
+$uri = $_SERVER['PHP_SELF'];
+define('BASE_URL', "$proto://$host$uri");
 
-if (isset($_GET['qwc'])) {
-  ?><<??>?xml version="1.0"?>
-<QBWCXML>
-	<AppName>QuickBooks XML Importer</AppName>
-	<AppID></AppID>
-	<AppURL>http://localhost.colin-virtual/server.php</AppURL>
-	<AppDescription>Import huge QBXML files in chunks.</AppDescription>
-	<AppSupport>http://localhost.colin-virtual/README.txt</AppSupport>
-	<UserName>username</UserName>
-	<OwnerID>{90A44FB7-33D9-4815-AC85-AC86A7E7D1EB}</OwnerID>
-	<FileID>{57F3B9B6-86F1-4FCC-B1FF-967DE1813D20}</FileID>
-	<QBType>QBFS</QBType>
-	<Scheduler>
-		<RunEveryNMinutes>2</RunEveryNMinutes>
-	</Scheduler>
-	<IsReadOnly>false</IsReadOnly>
-</QBWCXML><?
-  exit;
-}
-
-function debug($msg) { error_log(date('r').' '.$msg."\n", 3, __DIR__.'/debug.log'); }
-function error($msg) { error_log(date('r').' '.$msg."\n", 3, __DIR__.'/error.log'); }
+function debug($msg) { error_log(date('c').' '.$msg."\n", 3, VAR_DIR.'/debug.log'); }
+function error($msg) { error_log(date('c').' '.$msg."\n", 3, VAR_DIR.'/error.log'); }
 
 class QBServ {
+
+  const VERSION = 'v0.1.WTF';
 
   protected $fatalError;
   protected $config;
 
-  public function __construct($configPath)
+  public function __construct()
   {
-    if ( ! file_exists($configPath)) {
-      $this->fatalError = 'Could not load config.json';
-      return;
+    try {
+      $configPath = BP.'/config.txt';
+      if ( ! is_readable($configPath)) {
+        throw new Exception('Could not load config.txt');
+      }
+      $configStr = trim(file_get_contents($configPath));
+      list($path, $start, $end) = explode('|', $configStr);
+      if ( ! strlen($path) || ! strlen($start) || ! strlen($end)) {
+        throw new Exception('Could not parse config.txt. Expected format: /path/to/files*.xml|1|523');
+      }
+      $this->config = (object) array('path' => $path, 'start' => $start, 'end' => $end);
+      $path = $this->_getFile($this->config->start);
+      if ( ! is_readable($path)) {
+        throw new Exception("Cannot read $path.");
+      }
+      $path = $this->_getFile();
+      if ( ! touch($path)) {
+        throw new Exception("Cannot write to status file: $path");
+      }
+    } catch(Exception $e) {
+      $this->fatalError = $e->getMessage();
     }
-    $configStr = file_get_contents($configPath);
-    $config = @json_decode($configStr, true);
-    if ( ! $config) {
-      $this->fatalError = 'Could not parse config.json (path,start,end)';
-      return;
+    if (isset($_GET['qwc'])) {
+      if ($this->fatalError) {
+        die("<h3>{$this->fatalError}</h3>");
+      }
+      header('Content-Disposition: attachment; filename=qbserv.qwc');
+      header('Content-Type: text/xml');
+      ?><<??>?xml version="1.0"?>
+<QBWCXML>
+  <AppName>QuickBooks XML Importer</AppName>
+  <AppID></AppID>
+  <AppURL><?= BASE_URL ?></AppURL>
+  <AppDescription>Import huge QBXML files in chunks.</AppDescription>
+  <AppSupport><?= str_replace('server.php', 'README.txt', BASE_URL) ?></AppSupport>
+  <UserName>username</UserName>
+  <OwnerID>{90A44FB7-33D9-4815-AC85-AC86A7E7D1EB}</OwnerID>
+  <FileID>{57F3B9B6-86F1-4FCC-B1FF-967DE1813D20}</FileID>
+  <QBType>QBFS</QBType>
+  <Scheduler>
+    <RunEveryNMinutes>0</RunEveryNMinutes>
+  </Scheduler>
+  <IsReadOnly>false</IsReadOnly>
+</QBWCXML><?
+      exit;
     }
-    $this->config = $config;
-    $path = $this->_getFile($this->config->start);
-    if ( ! is_readable($path)) {
-      $this->fatalError = "Cannot read $path.";
-      return;
+  }
+
+  public function serverVersion($params)
+  {
+    debug("serverVersion");
+    return $this->_wrapResult(__FUNCTION__, self::VERSION);
+  }
+
+  public function clientVersion($params)
+  {
+    debug('clientVersion: '.$params->strVersion);
+    if ($this->fatalError) {
+      return $this->_wrapResult(__FUNCTION__, 'E:'.$this->fatalError);
     }
-    $path = $this->_getFile('-STATUS');
-    if ( ! touch($path)) {
-      $this->fatalError = "Cannot write to status file: $path";
-      return;
+    return $this->_wrapResult(__FUNCTION__, '');
+  }
+
+  public function authenticate($params)
+  {
+    debug("authenticate: $params->strUsername : $params->strPassword");
+    return $this->_wrapResult(__FUNCTION__, array(md5(time().$params->strUsername.$params->strPassword), ''));
+  }
+
+  public function closeConnection($params)
+  {
+    $this->_checkTicket($params->ticket);
+    if ($this->fatalError) {
+      return $this->_wrapResult(__FUNCTION__, 'FATAL ERROR: '.$this->fatalError);
     }
+    return $this->_wrapResult(__FUNCTION__, 'QBServ says goodbye!');
   }
 
-  public function authenticate($username, $password)
+  public function connectionError($params)
   {
-    return array(md5(time().$username.$password), '');
+    $this->_checkTicket($params->ticket);
+    error("connectionError: $params->message ($params->hresult)");
+    return $this->_wrapResult(__FUNCTION__, 'done');
   }
 
-  public function clientVersion($version)
+  public function getInteractiveURL($params)
   {
-    debug('clientVersion: '.$version);
-    return '';
-  }
-
-  public function closeConnection($ticket)
-  {
-    $this->_checkTicket($ticket);
-    return 'QBServ says goodbye!';
-  }
-
-  public function connectionError($ticket, $hresult, $message)
-  {
-    $this->_checkTicket($ticket);
-    error("connectionError: $message ($hresult)");
-    return 'done';
-  }
-
-  public function getInteractiveURL($ticket)
-  {
-    $this->_checkTicket($ticket);
+    $this->_checkTicket($params->ticket);
     error("getInteractiveURL not supported");
-    return 'http://twitter.com/';
+    return $this->_wrapResult(__FUNCTION__, BASE_URL.'?pi');
   }
 
-  public function getLastError($ticket)
+  public function getLastError($params)
   {
-    $this->_checkTicket($ticket);
-    $errfile = __DIR__.'/last_error-'.$ticket.'.txt';
+    $this->_checkTicket($params->ticket);
+    $errfile = VAR_DIR.'/last_error-'.$params->ticket.'.txt';
     if (file_exists($errfile)) {
       $err = file_get_contents($errfile);
       unlink($errfile);
-      return $err;
+      return $this->_wrapResult(__FUNCTION__, $err);
     } else {
-      return 'NoOp';
+      return $this->_wrapResult(__FUNCTION__, ''); // reply NoOp to pause for 5 seconds
     }
   }
 
-  public function getServerVersion($ticket)
+  public function interactiveDone($params)
   {
-    $this->_checkTicket($ticket);
-    return '0.1.WTF';
+    $this->_checkTicket($params->ticket);
+    return $this->_wrapResult(__FUNCTION__, 'Done');
   }
 
-  public function interactiveDone($ticket) { $this->_checkTicket($ticket); return 'Done'; }
-  public function interactiveRejected($ticket) { $this->_checkTicket($ticket); return 'WAT?'; }
-
-  public function receiveResponseXML($ticket, $response, $hresult, $message)
+  public function interactiveRejected($params)
   {
-    $this->_checkTicket($ticket);
-    if ($hresult || $message) {
-      error("receiveResponseXML: $message ($hresult)");
-      $this->_setLastError($ticket, "Aborting due to received response indicating Quickbooks error: $message");
-      return -1;
+    $this->_checkTicket($params->ticket);
+    return $this->_wrapResult(__FUNCTION__, 'WAT?');
+  }
+
+  public function sendRequestXML($params)
+  {
+    $this->_checkTicket($params->ticket);
+    if ($params->strHCPResponse) {
+      debug("New session: QBXML $params->qbXMLCountry $params->qbXMLMajorVers.$params->qbXMLMinorVers");
+      debug("strHCPResponse: $params->strHCPResponse");
+      debug("strCompanyFileName: $params->strCompanyFileName");
     }
 
-    // TODO handle $response
-    return 100;
+    $status = $this->_getStatus();
+    $part = null;
+    if ( ! strlen($status)) {
+      $part = $this->config->start;
+    } else if (is_numeric($status)) {
+      $part = $status + 1;
+    }
+
+    // Unrecognized status
+    if ($part === null) {
+      $this->_setLastError($params->ticket, 'Unrecognized status: '.$status);
+      return $this->_wrapResult(__FUNCTION__, 'NoOp');
+    }
+
+    // All done!
+    if ($part > $this->config->end) {
+      return $this->_wrapResult(__FUNCTION__, '');
+    }
+
+    // Send a part
+    $file = $this->_getFile($part);
+    $contents = file_get_contents($file);
+    if ( ! $contents) {
+      $this->_setLastError($params->ticket, 'Could not get file contents: '.$file);
+      return $this->_wrapResult(__FUNCTION__, 'NoOp');
+    }
+    $this->_setStatus($part);
+    $onError = 'onError="continueOnError" responseData="includeNone"';
+    $qbxml = <<<XML
+<?xml version="1.0" ?>
+<?qbxml version="2.1"?>
+<QBXML>
+  <QBXMLMsgsRq $onError>
+$contents  </QBXMLMsgsRq>
+</QBXML>
+XML;
+    return $this->_wrapResult(__FUNCTION__, $qbxml);
   }
 
-  public function sendRequestXML(
-    $ticket,
-    $strHCPResponse,
-    $strCompanyFileName,
-    $qbXMLCountry,
-    $qbXMLMajorVers,
-    $qbXMLMinorVers
-  )
+  public function receiveResponseXML($params)
   {
-    return '';
-    // TODO - NoOp for error
-    // TODO - QBXML string
+    $this->_checkTicket($params->ticket);
+    if ($params->hresult || $params->message) {
+      error("receiveResponseXML: $params->message ($params->hresult)");
+      $this->_setLastError($params->ticket, "Aborting due to received response indicating Quickbooks error: $params->message");
+      return $this->_wrapResult(__FUNCTION__, -1);
+    }
+
+    $percent = ($this->config->end - $this->config->start) / ($this->_getStatus() - $this->config->start);
+    $percent = min(100,max(0, ceil($percent)));
+    debug("receiveResponseXML ($percent%):\n$params->response");
+    return $this->_wrapResult(__FUNCTION__, $percent);
   }
 
   protected function _checkTicket($ticket)
@@ -142,21 +207,39 @@ class QBServ {
 
   protected function _setLastError($ticket, $message)
   {
-    $errfile = __DIR__.'/last_error-'.$ticket.'.txt';
+    $errfile = VAR_DIR.'/last_error-'.$ticket.'.txt';
     file_put_contents($errfile, $message) or error("Could not write to $errfile");
   }
 
-  protected function _getFile($num)
+  protected function _getFile($num = '-STATUS')
   {
     if ( ! $this->config->path) die("No path");
     return str_replace('*', $num, $this->config->path);
   }
 
+  protected function _wrapResult($type, $result)
+  {
+    #return (object) array($type.'Response' => (object) array($type.'Result' => $result));
+    $response = (object) array($type.'Result' => $result);
+    debug("RESPONSE ($type) ".print_r($response, true));
+    return $response;
+  }
+
+  protected function _getStatus()
+  {
+    return file_get_contents($this->_getFile());
+  }
+
+  protected function _setStatus($status)
+  {
+    return file_put_contents($this->_getFile(), $status);
+  }
+
 }
 
 try {
-  $qbserv = new QBServ($config);
-  $server = new SoapServer(__DIR__.'/QBWebConnectorSvc.wsdl');
+  $qbserv = new QBServ();
+  $server = new SoapServer(BP.'/QBWebConnectorSvc.wsdl');
   $server->setObject($qbserv);
   $server->handle();
 } catch (Exception $e) {
